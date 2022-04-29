69bd7549e868570b2d46062bb53f016be04e37c7
Gerard <geriloza@gmail.com>
2022-04-30 01:25:46 +0200

    project initialitzation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import LoginScreen from './Screens/Login';
+import RegisterScreen from './Screens/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/login' element={<LoginScreen/>}></Route>
+        <Route exact path='/register' element={<RegisterScreen/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
