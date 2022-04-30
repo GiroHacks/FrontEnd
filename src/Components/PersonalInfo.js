0d3d887f24ca7603dc09812dea309722eda103fb
Gerard <geriloza@gmail.com>
2022-04-30 12:12:39 +0200

    register done

---
+++ src/Components/PersonalInfo.js
@@ -0,0 +1,65 @@
+import { Container, TextField, IconButton, Alert, AlertTitle } from "@mui/material";
+import { ArrowForward } from "@mui/icons-material";
+import React from "react";
+import { useState } from "react";
+import axios from 'axios';
+
+
+export default function PersonalInfo(props){
+
+    const [firstName,setFirstName] = useState("")
+    const [lastName,setLastName] = useState("")
+    const [email,setEmail] = useState("")
+    const [phoneNum, setPhoneNum] = useState("")
+    const [birthDate,setBirthDate] = useState(undefined)
+    const [submited,setSubmited] = useState(false)
+    const [emailExists, setEmailExists] = useState(false)
+
+    const sendDataAndIncrease = async ()=>{
+        setSubmited(true)
+        if(firstName!=="" && lastName!=="" && email!=="" && phoneNum!=="" && birthDate!==undefined){
+            try{
+                let res = await axios.post("http://66.70.178.146:8001/api/checkemail",{"email":email})
+                if(res.status===200){
+                    setEmailExists(false)
+                    const userObject={
+                        firstName : firstName,
+                        lastName : lastName,
+                        email : email,
+                        phoneNumber : phoneNum,
+                        birthDate : birthDate
+                    }
+                    props.sendData(userObject)
+                    props.increase()
+                }
+                else setEmailExists(true)
+            }catch(e){
+                setEmailExists(true)
+            }
+        }
+    }
+
+    return(
+        <div className="formContainer">
+            <h2 style={{color:"#167db7"}} >PERSONAL INFORMATION</h2>
+            <div className="horizontalFlex">
+                <TextField error={firstName==="" && submited} value={firstName} onChange={(event)=>setFirstName(event.target.value)} style={{width:"47%"}}  label="First Name*" variant="outlined" />
+                <TextField error={lastName==="" && submited} value={lastName} onChange={(event)=>setLastName(event.target.value)} style={{width:"47%"}}  label="Last Name*" variant="outlined" />
+            </div>
+            <TextField error={(email==="" || emailExists) && submited} value={email} onChange={(event)=>setEmail(event.target.value)} style={{marginTop:"30px"}} type="email" fullWidth  label="Email*" variant="outlined" />
+            <div className="horizontalFlex" style={{marginTop:"30px"}}>
+                <TextField error={phoneNum==="" && submited}  value={phoneNum} onChange={(event)=>setPhoneNum(event.target.value)} style={{width:"47%"}}  label="Phone number*" variant="outlined" />
+                <TextField error={birthDate===undefined && submited} value={birthDate} onChange={(event)=>setBirthDate(event.target.value)} type="date" style={{width:"47%"}} variant="outlined" />
+            </div>
+            <div className="horizontalFlexEnd" style={{marginTop:"30px"}}>
+                <IconButton onClick={sendDataAndIncrease} aria-label="next" size="large">
+                    <ArrowForward fontSize="inherit" />
+                </IconButton>
+            </div>
+            <Alert severity="error" style={!emailExists?{display:"none"}:{display:"block",width:"60%"}}>
+                <AlertTitle>Error</AlertTitle>
+                This email already exists !
+            </Alert>
+        </div>
+    )
+}
--- src/Screens/Register.js
+++ src/Screens/Register.js
@@ -1,7 +1,98 @@
 import React from "react";
+import { useState,useEffect } from "react";
+import { Grid, Step,Stepper,StepLabel, Button, Box,Alert,AlertTitle } from "@mui/material";
+import personal_info from "../Images/personal_info.svg"
+import skills_image from "../Images/skills_image.svg"
+import password_image from "../Images/password_image.svg"
+import NavBar from "../Components/Navbar";
+import PersonalInfo from "../Components/PersonalInfo";
+import Aptitudes from "../Components/Aptitudes";
+import PasswordGenerator from "../Components/PasswordGenerator";
+import axios from "axios";
+
+
+const steps = [
+    'Personal information',
+    'Aptitudes',
+    'Password generation',
+];
 
 export default function RegisterScreen(){
+
+    const [stepCount,setStepCount] = useState(0);
+    const [registerUser,setUserRegister] = useState(null)
+    const [userSkills,setUserSkills] = useState(null)
+    const [unexpectedError,setUnexpectedError] = useState(false)
+
+    const increaseStepCount = () =>{
+        setStepCount(stepCount+1)
+    }
+    const submitUser = async (password)=>{
+        const postUser = {
+            "email":registerUser.email,
+            "password":password,
+            "first_name":registerUser.firstName,
+            "last_name":registerUser.lastName,
+            "phone":registerUser.phoneNumber,
+            "birthdate":registerUser.birthDate,
+            "skills":userSkills
+        }
+        try{
+            let res = await axios.post("http://66.70.178.146:8001/api/register",postUser)
+            if(res.status===200){
+                setUnexpectedError(false)
+                console.log("user posted")
+            }else{
+                setUnexpectedError(true)
+            }
+        }catch(e){
+            setUnexpectedError(true)
+        }
+    }
+    const handleUserPersonalData=(personalData)=>{
+        setUserRegister(personalData)
+    }
+    const handleUserSkills=(skills)=>{
+        setUserSkills(skills)
+    }
+
     return(
-        <h1>Register</h1>
+        <React.Fragment>
+            <NavBar/>
+            <Grid container className="parentGrid">
+                <Grid item xs={12} sm={12} lg={6} className="leftGridItem" display={{xs:"none",sm:"none",md:"none",lg:"flex"}}>
+                    <img className="rightImage" src={stepCount===0 ? personal_info : stepCount===1 ? skills_image : password_image }></img>
+                </Grid>
+                <Grid item xs={12} sm={12} lg={6} className="rightGridItem">
+                    <Box className="stepperBox">
+                        <Stepper activeStep={stepCount} alternativeLabel>
+                            {steps.map((label) => (
+                            <Step key={label}>
+                                <StepLabel>{label}</StepLabel>
+                            </Step>
+                            ))}
+                        </Stepper>
+                    </Box>
+                    <div className="contentDiv">
+                        {
+                            stepCount===0?(
+                                <PersonalInfo increase = {increaseStepCount} sendData={handleUserPersonalData}/>
+                            ):stepCount===1?(
+                                <Aptitudes increase = {increaseStepCount} sendData = {handleUserSkills} />
+                            ):(
+                                <PasswordGenerator submit = {submitUser}/>
+                            )
+                            
+                        }
+                        <Alert severity="error" style={!unexpectedError?{display:"none"}:{display:"block",width:"45%"}}>
+                            <AlertTitle>Error</AlertTitle>
+                            Some unexpected error ocurred on our server!
+                        </Alert>
+                    </div>
+                    
+                </Grid>
+            </Grid>
+        </React.Fragment>
+        
     )
 }
