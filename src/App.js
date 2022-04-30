dd893c076ef1a2c79429cc7fa033a19eb116abce
n0ss4 <nghazouani@outlook.com>
2022-04-30 07:15:53 +0200

    Login funcional amb JWT

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import LoginScreen from './Screens/Login';
 import RegisterScreen from './Screens/Register';
+import HomeScreen from './Screens/Home';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import PrivateRoute from "./PrivateRoute";
 
 const theme = createTheme({
   palette: {
@@ -18,8 +20,11 @@ function App() {
     <ThemeProvider theme={theme}>
       <Router>
         <Routes>
-          <Route exact path='/login' element={<LoginScreen/>}></Route>
-          <Route exact path='/register' element={<RegisterScreen/>}></Route>
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/home" element={<HomeScreen />} />
+          </Route>
+          <Route exact path='/login' element={<LoginScreen/>}/>
+          <Route exact path='/register' element={<RegisterScreen/>}/>
         </Routes>
       </Router>
     </ThemeProvider>
--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -3,9 +3,13 @@ import { Grid, Box } from "@mui/material";
 import loginImage from "../img/undraw_login_re_4vu2.svg";
 import loginLogoImage from "../img/logo.png";
 import LoginForm from "../Components/LoginForm";
+import AuthService from "../Services/AuthService";
+import { Navigate } from "react-router-dom";
 
-export default function LoginScreen(){
-    return(
+export const Auth = new AuthService();
+
+export const Login = () => {
+    return (
         <Grid container className="loginGrid">
             <Grid item lg={6} className="leftLoginGrid" display={{ xs: "none", sm: "none", md: "flex"}}>
                 <Box className="leftLoginImageBox">
@@ -27,4 +31,12 @@ export default function LoginScreen(){
             </Grid>
         </Grid>
     )
+}
+
+export default function LoginScreen(){
+    if(Auth.loggedIn()){
+        return <Navigate to="/home"/>
+    }else{
+        return <Login/>
+    }
 }
